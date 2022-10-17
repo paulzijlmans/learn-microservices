12d87cfe5c937e51ba8b28834a32b91eada3d6a3
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-17 08:58:38 +0200

    Add log statements and ip address properties

--- gamification/src/main/java/microservices/book/gamification/game/LeaderBoardController.java
+++ gamification/src/main/java/microservices/book/gamification/game/LeaderBoardController.java
@@ -2,11 +2,13 @@ package microservices.book.gamification.game;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.gamification.game.domain.LeaderBoardRow;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/leaders")
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ public class LeaderBoardController {
 
     @GetMapping
     public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Retrieving leaderboard");
         return leaderBoardService.getCurrentLeaderBoard();
     }
 }
